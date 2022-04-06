d08e8701044731947910939518614b91ef043027
Adrián Ramón <adrianramon2003@gmail.com>
2022-04-06 13:37:30 +0200

    Añadido script

---
+++ script.js
@@ -0,0 +1,8 @@
+function obtenerDatos(){
+    var nombre=document.getElementById('nombre').value
+    var edad=document.getElementById('edad').value
+
+    document.registro.nombreobtenido.value = nombre;
+    document.registro.edadobtenida.value = edad;
+
+}
